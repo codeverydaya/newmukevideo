b50501aab52cc2471d00c152863055303152d7a6
lmy-hub <1013127768@qq.com>
2019-11-16 15:48:30 +0800

    new

--- android/app/src/main/java/com/newmukevideo/MainActivity.java
+++ android/app/src/main/java/com/newmukevideo/MainActivity.java
@@ -1,6 +1,9 @@
 package com.newmukevideo;
 
 import com.facebook.react.ReactActivity;
+import com.facebook.react.ReactActivityDelegate;
+import com.facebook.react.ReactRootView;
+import com.swmansion.gesturehandler.react.RNGestureHandlerEnabledRootView;
 
 public class MainActivity extends ReactActivity {
 
@@ -11,5 +14,14 @@ public class MainActivity extends ReactActivity {
   @Override
   protected String getMainComponentName() {
     return "newmukevideo";
+  }
+  @Override
+  protected ReactActivityDelegate createReactActivityDelegate() {
+    return new ReactActivityDelegate(this, getMainComponentName()) {
+      @Override
+      protected ReactRootView createRootView() {
+        return new RNGestureHandlerEnabledRootView(MainActivity.this);
+      }
+    };
   }
 }
